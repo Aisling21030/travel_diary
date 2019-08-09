411e9c3d7f4fe5d819c1f5ce4dd6cb9c97655d91
Aisling Corcoran <aislingcorcoran21030@gmail.com>
2019-08-09 10:57:33 +0100

    add styling

--- db/migrate/20190807103944_add_name_to_cities.rb
+++ db/migrate/20190807103944_add_name_to_cities.rb
@@ -1,5 +1,8 @@
 class AddNameToCities < ActiveRecord::Migration[5.2]
   def change
     add_column :cities,  :name, :string
+    t.string :title
+    t.text :body
+    t.references :city, foreign_key: true
   end
 end
